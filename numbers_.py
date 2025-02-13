1c9825ef9fa4f30030664951efc7baeeb7aefcdd
Felix Dzerzhinsky <heil@meridiant.ru>
2025-02-13 14:55:59 +0500

    1-7 уроки

--- numbers_.py
+++ numbers_.py
@@ -3,4 +3,7 @@ import numpy as np
 import imutils, easyocr
 from matplotlib import pyplot as plt
 
-img = cv2.imread('images/image1.webp')
+img = cv2.imread('images/num1.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+plt.imshow(img)
+plt.show()
