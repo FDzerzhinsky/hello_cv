9c63a5be4f898f98e7f49fc8ca3cc044159b51ec
Felix Dzerzhinsky <heil@meridiant.ru>
2025-02-13 14:59:48 +0500

    Облегченная версия для пробного развёртывания на DL

--- numbers_.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-import imutils, easyocr
-from matplotlib import pyplot as plt
-
-img = cv2.imread('images/num1.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-plt.imshow(img)
-plt.show()
